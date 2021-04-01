2ba9a51928063f3261e8fbfa45d206a810cea3bb
shljshlj <34565607+shljshlj@users.noreply.github.com>
2021-04-01 23:56:59 +0200

    Fix color styling for header and footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,21 +1,24 @@
-import { Box, Link } from '@chakra-ui/react';
+import { Flex, Box, Link } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <footer>
-      <Box h="40px">
-        &copy;
-    <Link
-          color="teal.500"
-          href="https://github.com/shljshlj"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          shljshlj
-    </Link>
-    2021
+    <Box
+      as="footer"
+      bg="gray.700"
+      color="white"
+    >
+      <Flex
+        w="100%"
+        maxW="1200px"
+        p="1.5rem 1rem"
+        mx="auto"
+        h="100%"
+      >
+        <Box>
+          &copy; <Link color="teal.200" href="https://github.com/shljshlj" target="_blank" rel="noopener noreferrer">shljshlj</Link> 2021
+        </Box>
+      </Flex>
     </Box>
-    </footer>
   );
 };
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,14 +2,16 @@ import Logo from '../logo/logo.component';
 import SiteNav from '../site-nav/site-nav.component';
 import { ColorModeSwitcher } from '../color-mode/ColorModeSwitcher';
 
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, useColorMode } from '@chakra-ui/react';
 
 const Header = () => {
+  const { colorMode } = useColorMode();
   return (
     <Box
       as="header"
-      bg="teal.500"
-      color="white"
+      bg={colorMode === "light" ? "white" : "gray.900"}
+      color={colorMode === "light" ? "black" : "white"}
+      boxShadow="rgba(0, 0, 0, 0.1) 0px 1px 2px 0px"
     >
       <Flex
         justify="space-between"
