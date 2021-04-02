8fcd6f1818937ecff757fddb0a220c06c9373487
shljshlj <34565607+shljshlj@users.noreply.github.com>
2021-04-02 02:21:07 +0200

    Update styling of login page with chakra ui components

--- src/components/layouts/page-container.component.jsx
+++ src/components/layouts/page-container.component.jsx
@@ -2,7 +2,10 @@ import { Box } from "@chakra-ui/layout"
 
 const PageContainer = (props) => {
   return (
-    <Box as="main" flex="1">
+    <Box
+      as="main"
+      flex="1"
+    >
       {props.children}
     </Box>
   );
--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,5 +1,27 @@
 import useCustomForm from '../../hooks/useCustomForm';
 
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+  Box,
+  Flex,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+} from '@chakra-ui/react';
+
 const initialValues = {
   email: ''
 };
@@ -18,22 +40,83 @@ const LoginPage = ({ setToken }) => {
   });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <p>Email</p>
-        <input
-          name="email"
-          type="email"
-          value={values.email}
-          onChange={handleChange}
-          required
-        />
-        {!values.email && <span>Please enter an email address</span>}
-      </label>
-      <div>
-        <button type="submit">Sign In</button>
-      </div>
-    </form>
+    <Flex
+      w="100%"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      px={8}
+      bg="#fffaf6"
+    >
+      <Flex
+        direction="column"
+        alignItems="stretch"
+        w="100%"
+        maxWidth="500px"
+        borderRadius="lg"
+        borderWidth="1px"
+        bg="white"
+        p={8}
+        mt="-50%"
+      >
+        <Box
+          textAlign="center"
+          mt="1rem"
+          mb="3rem"
+        >
+          <Heading
+            as="h1"
+            mb="0.5rem"
+          >
+            Users App
+        </Heading>
+          <Text fontSize="md">
+            Sign in with email to manage users data
+        </Text>
+        </Box>
+
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={6} alignItems="stretch">
+            <FormControl id="email" isRequired>
+              <FormLabel>Email Address</FormLabel>
+              <Input
+                required
+                name="email"
+                type="email"
+                placeholder="Enter your email address"
+                value={values.email}
+                onChange={handleChange}
+              />
+            </FormControl>
+
+            <Button colorScheme="teal" type="submit">Sign In</Button>
+
+            <Divider />
+
+            <Popover>
+              <PopoverTrigger>
+                <Link fontSize="sm" textAlign="center">Forgot your email?</Link>
+              </PopoverTrigger>
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader
+                  px={4}
+                  pt={4}
+                  fontWeight="bold"
+                  fontSize="sm"
+                  borderBottom="1px"
+                  borderColor="teal.100"
+                >
+                  Email used to login:
+                </PopoverHeader>
+                <PopoverBody p={4}>someemail@mail.com</PopoverBody>
+              </PopoverContent>
+            </Popover>
+          </Stack>
+        </form>
+      </Flex>
+    </Flex>
   );
 }
 
