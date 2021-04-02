3639ddfaa7b69c72940fdf3874b42e6f614ff383
shljshlj <34565607+shljshlj@users.noreply.github.com>
2021-04-02 15:28:59 +0200

    Update demo branch with 404.html and redirect script inside index.html in public folder, to enable deployment and routing on gh-pages

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/users-app">
         <PageLayout>
           <Header />
           <PageContainer>
