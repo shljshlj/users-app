b1712a511271a413b9ecd883fe25eff35e1b3294
shljshlj <34565607+shljshlj@users.noreply.github.com>
2021-04-04 03:36:24 +0200

    Add username, job title and profile picture components to user-details page

--- src/pages/user-details/user-details.component.jsx
+++ src/pages/user-details/user-details.component.jsx
@@ -4,8 +4,72 @@ import { userService } from '../../services/userService';
 
 import PageContent from '../../components/layouts/page-content.component';
 
-import { Box, Flex, Heading, Image, SimpleGrid, Spinner, Square, StackDivider, VStack } from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, Grid, Square, StackDivider, VStack, GridItem, Text } from '@chakra-ui/react';
 
+
+const UserHeading = ({ fullName }) => {
+  return (
+    <Heading
+      as="h1"
+      size="xl"
+      letterSpacing="0.1em"
+      mt="1em"
+    >
+      {fullName}
+    </Heading>
+  );
+};
+
+const JobTitle = ({ jobTitle }) => {
+  return (
+    <Text
+      mt="0.5em"
+      color="gray"
+      fontWeight="bold"
+    >
+      {jobTitle}
+    </Text>
+  );
+};
+
+const ProfileImage = ({ avatar, fullName }) => {
+  return (
+    <Square
+      mx="auto"
+      maxW="170px"
+      size={{ "sm": "170px" }}
+      borderWidth="4px"
+      borderRadius="8px"
+      overflow="hidden"
+    >
+      <Image
+        loading="lazy"
+        boxSize="100%"
+        objectFit="cover"
+        src={avatar || "https://via.placeholder.com/150"}
+        fallbackSrc="https://via.placeholder.com/150"
+        alt={`Profile picture of ${fullName}`}
+      />
+    </Square>
+  );
+};
+
+const ContentDivider = ({ dividerText }) => {
+  return (
+    <Box
+      borderBottom="1px"
+      borderColor="gray.700"
+      textTransform="uppercase"
+      fontSize="lg"
+      py={2}
+      mx={{ "base": "1em", "md": "0" }}
+      mt="2em"
+      textAlign={{ "base": "center", "md": "unset" }}
+    >
+      {dividerText}
+    </Box>
+  );
+};
 
 const UserDetailsPage = () => {
   const { id } = useParams();
@@ -30,41 +94,44 @@ const UserDetailsPage = () => {
     );
   }
 
+
+
+
   return (user &&
     <PageContent>
-      <SimpleGrid width="100%" columns={{ md: 2 }}>
-        <VStack
-          divider={<StackDivider borderColor="gray.200" />}
-          spacing={8}
+      <Box
+        borderWidth="1px"
+        borderRadius="lg"
+        p={4}
+        width="100%"
+        boxShadow="0 1px 2px rgba(0, 0, 0, 0.2)"
+      >
+        <Grid
+          templateColumns={{ "base": "1fr", "md": "1fr 2fr" }}
+          // gap={4}
+          my="2rem"
+          justifyItems={{ "base": "center", "md": "unset" }}
         >
-          <Flex
-            direction="column"
+          <GridItem>
+            <ProfileImage avatar={user.avatar} fullName={user.fullName} />
+          </GridItem>
+          <GridItem>
+            <Flex
+              direction="column"
+              alignItems={{ "base": "center", "md": "flex-start" }}
+            >
+              <UserHeading fullName={user.fullName} />
+              <JobTitle jobTitle="Senior Manager" />
+            </Flex>
+          </GridItem>
+          <GridItem
+            colStart={{ "md": 2 }}
+            justifySelf="stretch"
           >
-            <Box mx="auto">
-              <Square size="130px" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                <Image
-                  loading="lazy"
-                  boxSize="100%"
-                  objectFit="cover"
-                  src={user.avatar || "https://via.placeholder.com/100"}
-                  fallbackSrc="https://via.placeholder.com/150"
-                  alt={`Profile picture of ${user.fullName}`}
-                />
-              </Square>
-            </Box>
-            <Box>
-              <Heading
-                as="h1"
-                size="lg"
-              >
-                {user.fullName}
-              </Heading>
-            </Box>
-          </Flex>
-
-        </VStack>
-
-      </SimpleGrid>
+            <ContentDivider dividerText={'Contact Details'} />
+          </GridItem>
+        </Grid>
+      </Box>
     </PageContent>
   );
 }
