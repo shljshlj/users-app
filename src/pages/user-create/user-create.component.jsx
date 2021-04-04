eab606b438563d26fc2591144bef3b9863476eac
shljshlj <34565607+shljshlj@users.noreply.github.com>
2021-04-04 07:19:12 +0200

    Add manually form fields for create new user form

--- src/pages/user-create/user-create.component.jsx
+++ src/pages/user-create/user-create.component.jsx
@@ -1,5 +1,32 @@
 import useCustomForm from '../../hooks/useCustomForm';
 
+import PageContent from '../../components/layouts/page-content.component';
+import ContentSection from '../../components/layouts/content-section.component';
+import PageHeading from '../../components/page-heading/page-heading.component';
+
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+  Box,
+  Flex,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  useColorMode
+} from '@chakra-ui/react';
+
 const initialValues = {
   fullName: '',
   email: '',
@@ -7,7 +34,7 @@ const initialValues = {
   country: ''
 };
 
-const UserCreatePage = () => {
+const UserCreateForm = () => {
   const {
     values,
     errors,
@@ -22,50 +49,135 @@ const UserCreatePage = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        <p>Full Name</p>
-        <input
-          name="fullName"
-          type="text"
-          value={values.fullName}
-          onChange={handleChange}
-          required
-        />
-        {!values.fullName && <span>Please enter a full name</span>}
-      </label>
-      <label>
-        <p>Email address</p>
-        <input
-          name="email"
-          type="email"
-          value={values.email}
-          onChange={handleChange}
-          required
-        />
-        {!values.email && <span>Please enter an email address</span>}
-      </label>
-      <label>
-        <p>City</p>
-        <input
-          name="city"
-          type="city"
-          value={values.city}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        <p>Country</p>
-        <input
-          name="country"
-          type="country"
-          value={values.country}
-          onChange={handleChange}
-        />
-      </label>
-      <div>
-        <button type="submit">Create New User</button>
-      </div>
+      <Stack spacing={6} alignItems="stretch">
+        <FormControl id="fullName" isRequired>
+          <FormLabel>Full Name</FormLabel>
+          <Input
+            required
+            name="fullName"
+            type="text"
+            placeholder="Full Name"
+            value={values.fullName}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="email" isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input
+            required
+            name="email"
+            type="email"
+            placeholder="Email address"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="website">
+          <FormLabel>Website</FormLabel>
+          <Input
+            name="website"
+            type="url"
+            placeholder="Personal website"
+            value={values.website}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="phone">
+          <FormLabel>Phone</FormLabel>
+          <Input
+            name="phone"
+            type="number"
+            placeholder="Phone number"
+            value={values.phone}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="jobTitle">
+          <FormLabel>Job Title</FormLabel>
+          <Input
+            name="jobTitle"
+            type="text"
+            placeholder="Job title"
+            value={values.jobTitle}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="street">
+          <FormLabel>Street</FormLabel>
+          <Input
+            name="street"
+            type="text"
+            placeholder="Street"
+            value={values.street}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="city">
+          <FormLabel>City</FormLabel>
+          <Input
+            name="city"
+            type="text"
+            placeholder="City"
+            value={values.city}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="state">
+          <FormLabel>State</FormLabel>
+          <Input
+            name="state"
+            type="text"
+            placeholder="State"
+            value={values.state}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="zip">
+          <FormLabel>ZIP Code</FormLabel>
+          <Input
+            name="zip"
+            type="text"
+            placeholder="ZIP code"
+            value={values.zip}
+            onChange={handleChange}
+          />
+        </FormControl>
+
+        <FormControl id="avatar">
+          <FormLabel>Picture URL</FormLabel>
+          <Input
+            name="avatar"
+            type="getUserDetails"
+            placeholder="Picture URL"
+            value={values.avatar}
+            onChange={handleChange}
+          />
+        </FormControl>
+      </Stack>
     </form>
+  )
+}
+
+const UserCreatePage = () => {
+
+  return (
+    <PageContent>
+      <PageHeading>
+        Create New User
+      </PageHeading>
+      <ContentSection>
+        <UserCreateForm />
+      </ContentSection>
+    </PageContent>
+
   );
 }
 
